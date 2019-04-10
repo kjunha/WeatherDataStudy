8a28122953cbccc23fd46d6eeb202086f41e45a4
kjunha <46745992+kjunha@users.noreply.github.com>
2019-04-09 14:35:32 -1000

    Weather Data Download Sequence Ready, Set up for server

--- cwdapicall.py
+++ cwdapicall.py
@@ -1,7 +1,23 @@
 import requests
+import datetime as d
 # Current Weather Data (www.openweathermap.org)
 
+#mytoken = '52c40593fe3f9f62839ef2765f0f8340'
+#r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=laie,us&units=imperial&mode=precipitation&appid=52c40593fe3f9f62839ef2765f0f8340')
+#print(r.json())
+now = d.datetime.now()
+y = str(now.year)
+m = str(now.month)
+d = str(now.day)
+sysdate = y + "/" + m + "/" + d
+hu = str(now.hour)
+mu = str(now.minute)
+if len(hu) < 2 :
+    while len(hu) < 2:
+        hu = "0" + hu
+if len(mu) < 2 :
+    while len(mu) < 2:
+        mu = "0" + mu
+systime = hu + ":" + mu
 
-mytoken = '52c40593fe3f9f62839ef2765f0f8340'
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=laie,us&units=imperial&appid=52c40593fe3f9f62839ef2765f0f8340')
-print(r.json()['clouds']['all'])
+print(sysdate + " " + systime)
--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+import requests
+import datetime as d
 
-temp
-cloud
+# Current Weather Data (www.openweathermap.org)
+mytoken = '52c40593fe3f9f62839ef2765f0f8340'
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=laie,us&units=imperial&mode=precipitation&appid=52c40593fe3f9f62839ef2765f0f8340')
+db = r.json()
+ext = str(db['main']['temp']) + "," + str(db['main']['humidity']) + "," + str(db['wind']['speed']) + "," + str(db['clouds']['all']) + "," + str(db['weather'][0]['description'])
 
+#System Time Call
+now = d.datetime.now()
+y = str(now.year)
+m = str(now.month)
+d = str(now.day)
+sysdate = y + "/" + m + "/" + d
+hu = str(now.hour)
+mu = str(now.minute)
+if len(hu) < 2 :
+    while len(hu) < 2:
+        hu = "0" + hu
+if len(mu) < 2 :
+    while len(mu) < 2:
+        mu = "0" + mu
+systime = hu + ":" + mu
 
-def getWeather():
-    mytoken = '52c40593fe3f9f62839ef2765f0f8340'
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=laie,us&units=imperial&appid=52c40593fe3f9f62839ef2765f0f8340')
-    print(r.json()['main']['temp'])
-
-def writeFile():
+f = open("weather_data_daily.csv", "a+")
+append = sysdate + "," + systime + "," + ext
+f.write(append + "\n")
+f.close()
